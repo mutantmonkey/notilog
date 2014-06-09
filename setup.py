c1e01b4375ae8f2975d36989f853f8c9232dd381
mutantmonkey <mutantmonkey@mutantmonkey.in>
2014-06-08 20:04:53 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="notilog",
+    packages=["notilog"],
+    scripts=["notilog-server"],
+    version="1.0",
+    description="notilog lets you know about naughty things that happen in your logs.",
+    license="ISC",
+    author="mutantmonkey",
+    author_email="notilog@mutantmonkey.in",
+    url="https://github.com/mutantmonkey/notilog",
+    install_requires=["amqp>=1.4.5", "kombu>=3.0.16", "PyYAML>3.11"]
+)
